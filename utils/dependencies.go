6f42f3fde13edbe39b04e21b312c54d3f745d060
Jose Lopez <lopezarizajosealejandro@gmail.com>
2023-03-12 12:09:24 -0500

    dependency util

---
+++ utils/dependencies.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"fmt"
+	"reflect"
+)
+
+var messageError = "there's no dependency with token %s"
+
+func InitDependencies(object interface{}) {
+	element := reflect.ValueOf(object).Elem()
+	for i := 0; i < element.NumField(); i++ {
+		field := element.Field(i)
+		if field.CanSet() {
+			token := field.Type().Name()
+			var dependency interface{}
+			dependency, err := Get(token, SERVICES)
+			if err == nil {
+				continue
+			}
+
+			dependency, err = Get(token, REPOSITORIES)
+			if err != nil {
+				panic(fmt.Errorf(messageError, token))
+			}
+
+			field.Set(reflect.ValueOf(dependency))
+		}
+	}
+}
